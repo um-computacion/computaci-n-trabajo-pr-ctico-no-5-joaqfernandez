3eee4f6fabc66353b9a7b375243f8fad59bdeb36
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-04-30 10:58:02 -0300

    agrego repr

--- src/alumno.py
+++ src/alumno.py
@@ -4,4 +4,6 @@ class Alumno(Persona):
     def __init__(self, nombre, apellido, edad, dni, legajo):
         super().__init__(nombre, apellido, edad, dni)
         self.legajo = legajo
-        
+
+    def __repr__(self):
+        return f"Alumno: {self.nombre} {self.apellido}, Edad: {self.edad}, DNI: {self.dni}, Legajo: {self.legajo}"
