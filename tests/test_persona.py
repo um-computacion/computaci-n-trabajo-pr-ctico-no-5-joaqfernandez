711bb92ff394e9552a959b241828df5f256da548
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-04-30 09:34:38 -0300

    agrego el test de repr persona

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -7,3 +7,8 @@ class testPersona(unittest.TestCase):
         self.assertEqual(persona.apellido, "Fernandez")
         self.assertEqual(persona.edad, 21)
         self.assertEqual(persona.dni, 423312351)
+
+    def repr_persona(self):
+        persona = Persona("Joaquin", "Fernandez", 21, 423312351)
+        expected_repr = "Persona: Joaquin Fernandez. Edad: 21. DNI: 423312351"
+        self.assertEqual(repr(persona), expected_repr)
