c389ee4557807d67b547d1886b127b4d7ede862d
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-04-30 09:51:49 -0300

    agrego pensamientos a la persona

--- src/persona.py
+++ src/persona.py
@@ -4,6 +4,8 @@ class Persona():
         self.apellido = apellido
         self.edad = edad
         self.dni = dni
+        self.pensamiento = 0
+        self.ultima_idea = "<Aun no piensa>"
         
     def __repr__(self):
         return f"Persona: {self.nombre} {self.apellido}. Edad: {self.edad} DNI: {self.dni}"
