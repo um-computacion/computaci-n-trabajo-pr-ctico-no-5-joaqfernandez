8c33b5ea924815764a1886e23fa04fafdd95e41e
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-04-30 10:57:48 -0300

    agrego test repr

--- tests/test_alumno.py
+++ tests/test_alumno.py
@@ -10,4 +10,7 @@ class testAlumno(unittest.TestCase):
         self.assertEqual = alumno.dni = 421235
         self.assertEqual = alumno.legajo = 123
 
-    
+    def test_repr_alumno(self):
+        alumno = Alumno("Pepe", "Ramon", 21, 421235, 123)
+        expected_repr = "Alumno: Pepe Ramon, Edad: 21, DNI: 421235, Legajo: 123"
+        self.assertEqual(str(alumno), expected_repr)
